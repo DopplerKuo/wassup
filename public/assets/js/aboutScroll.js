5442405e66ed5158352577f26075769b801c1ccf
DopplerKuo <doppler.kuo@gmail.com>
2017-11-22 17:43:14 +0800

    online

--- public/assets/js/aboutScroll.js
+++ public/assets/js/aboutScroll.js
@@ -2,7 +2,7 @@ $(function(){
 	$(window).bind('scroll resize', function(){
 		var $this = $(this);
 		var $this_Top = $this.scrollTop();
-		var $barFixedHeight = $('.sec1').height();
+		var $barFixedHeight = $('.sec1').height() / 2;
 
 		if($this_Top < $barFixedHeight){
 　　　 $('.fix-bg').removeClass('blur');
--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,18 +1,84 @@
-$(window).load(function() {
+$(document).ready(function() {
+
+	// animateNumbers
+	
+    $.fn.animateNumbers = function(stop, commas, duration, ease) {
+      return this.each(function() {
+	      var $this = $(this);
+        var start = parseInt($this.text().replace(/,/g, ""));
+	      commas = (commas === undefined) ? true : commas;
+        $({value: start}).animate({value: stop}, {
+        	duration: duration == undefined ? 1000 : duration,
+        	easing: ease == undefined ? "swing" : ease,
+					step: function() {
+        		$this.text(Math.floor(this.value));
+        		if (commas) { $this.text($this.text().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")); }
+      		},
+					complete: function() {
+		     	 	if (parseInt($this.text()) !== stop) {
+			      	$this.text(stop);
+							if (commas) { $this.text($this.text().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")); }
+						}
+        	}
+  		  });
+	    });
+    };
+  
+  // animateNumbers End
+
+	// loading
+  var progress = 0;
+  var $img_count = $("img").length;
+  $("img").on('load', function() {
+    progress += 100 / $img_count
+    var percent = Math.round(progress)
+    $("#loading .progress-text b").animateNumbers(percent, 100);
+    $("#loading .progress").css('width', percent + '%')
+  });
+
+  $(window).on('load', function() {
+  	$("#loading .progress-text b").animateNumbers(100, 100);
+  	$("#loading").addClass('completed')
+  })
+
 	$(".menu-btn").click(function() {
 		$(this).toggleClass('on');
 		$('.nav-list').toggleClass('on');
 	});
-})
 
-$(document).ready(function() {
+	$(".mobile-icon").click(function() {
+		$(".mobile-list").slideToggle(300);
+		$(".header-nav").toggleClass('active')
+	});
+
+	$(document).on('click', 'a[href^="#"]', function (event) {
+	    event.preventDefault();
+	    $('html, body').animate({
+	        scrollTop: $($.attr(this, 'href')).offset().top
+	    }, 500);
+	});
+
+	$(".backtop").click(function () {
+	   $("html, body").animate({scrollTop: 0}, 1000);
+	});
+
+	window.sr = ScrollReveal({ reset: false });
+	sr.reveal('.reveal', { 
+		duration: 600,
+		delay: 400
+	});
+
   // vue
   Wassup = new Vue ({
     el: 'body',
     data: {
     	isShow: false,
       wantToBuyItemId: 3722,
-      wantToBuyItemQuantity: 1
+      wantToBuyItemQuantity: 1,
+      popup: {
+      	whichMore: 1,
+      	isShow: false
+      }
     },
 
     methods: {
@@ -20,9 +86,13 @@ $(document).ready(function() {
     		this.isShow = !this.isShow
     	},
 
+    	showPopup: function(which) {
+    		this.popup.isShow = true;
+    		this.popup.whichMore = which
+    	},
+
     	changeWantToBuyItemId: function(id) {
     		this.wantToBuyItemId = id
-    		console.log(this.wantToBuyItemQuantity)
     	},
     	
       formSubmit: function() {
